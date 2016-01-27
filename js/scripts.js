120cde5bbd99d7e4b7b9ec59e8e9ea3663a9ba43
Kyle Derrick and paul hess <pauljacksonhess@gmail.com>
2016-01-27 11:10:13 -0800

    add Board constructor passing specs

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,22 @@ function Space(xCoordinate, yCoordinate) {
   this.xCoordinate = xCoordinate;
   this.yCoordinate = yCoordinate;
 };
+
+Space.prototype.markedBy = function(player) {
+  return player.mark;
+};
+
+function Board() {
+  this.grid = [];
+};
+
+Board.prototype.gridMaker = function() {
+  for (var i = 1; i <= 3; i++) {
+    var newSpace = new Space(1, i);
+    var newSpace2 = new Space(2, i);
+    var newSpace3 = new Space(3, i);
+    this.grid.push(newSpace, newSpace2, newSpace3);
+  };
+  console.log(this.grid);
+  return this.grid;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -6,12 +6,45 @@ describe('Player', function() {
 });
 
 describe('Space', function() {
-    it("returns the player's mark", function() {
+  it("returns the player's coordinate of x", function() {
       var testSpace = new Space(1,2);
       expect(testSpace.xCoordinate).to.equal(1);
    });
-   it("returns the player's mark", function() {
+
+  it("returns the player's coordinate of y", function() {
      var testSpace = new Space(1,2);
      expect(testSpace.yCoordinate).to.equal(2);
+  });
+
+  it("lets a player mark a space", function() {
+     var testPlayer = new Player("X")
+     var testSpace = new Space(1,2);
+     expect(testSpace.markedBy(testPlayer)).to.equal(testPlayer.mark);
+  });
+
+});
+
+describe('Board', function() {
+  it('expect Board to be a constructor with one empty array property', function() {
+    var newBoard = new Board();
+    expect(newBoard.grid).to.eql([])
+  });
+
+  it('expect thisBoard.gridMaker to make a grid of nine spaces', function() {
+    var newBoard = new Board();
+    newBoard.gridMaker();
+    expect(newBoard.grid.length).to.equal(9);
+  });
+
+  it('expect thisBoard.gridMaker create the specified coordinates of x', function() {
+    var newBoard = new Board();
+    newBoard.gridMaker();
+    expect(newBoard.grid[4].xCoordinate).to.equal(2);
+  });
+
+  it('expect thisBoard.gridMaker create the specified coordinates of y', function() {
+    var newBoard = new Board();
+    newBoard.gridMaker();
+    expect(newBoard.grid[7].yCoordinate).to.equal(3);
   });
 });
